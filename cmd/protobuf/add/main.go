f734cc4cbf517e04267cda5d68314fb6cadddf5c
longyue0521 <longyueli0521@gmail.com>
2021-10-12 23:38:11 +0800

    feat(protobuf): add main for add and list addressbook
    cd cmd/protobuf
    
    go run add/main.go addressbook.data to add person
    go run list/main.go addressbook.data to list person

---
+++ cmd/protobuf/add/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	pb "github.com/gojustforfun/protobuf-grpc-gateway-example/protobuf/example"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage:  %s ADDRESS_BOOK_FILE\n", os.Args[0])
+	}
+	fname := os.Args[1]
+
+	in, err := os.ReadFile(fname)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("%s: File not found.  Creating new file.\n", fname)
+		} else {
+			log.Fatalln("Error reading file:", err)
+		}
+	}
+	book := &pb.AddressBook{}
+	if err := proto.Unmarshal(in, book); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
+	}
+
+	addr, err := pb.PromptForAddress(os.Stdin)
+	if err != nil {
+		log.Fatalln("Error with address:", err)
+	}
+	book.People = append(book.People, addr)
+
+	out, err := proto.Marshal(book)
+	if err != nil {
+		log.Fatalln("Failed to encode address book:", err)
+	}
+	if err := os.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+}
